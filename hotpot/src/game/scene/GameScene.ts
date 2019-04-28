048d05c181012bf43e45cf41f302f73c5a597b9a
Liu Yang <yang.liu@anansimobile.org>
2019-04-29 01:03:13 +0800

    [annotation] add annotation

--- hotpot/src/game/scene/GameScene.ts
+++ hotpot/src/game/scene/GameScene.ts
@@ -10,6 +10,7 @@ class GameScene extends BaseScene {
     public onEnter() {
         super.onEnter();
 
+        // TODO: 时间较紧张，后期可以封装stageManger
         // this.addLayer(LayerManager.Game_Bg);//视情况添加
         this.addLayer(LayerManager.Game_Main);
         this.addLayer(LayerManager.UI_PopUp);
